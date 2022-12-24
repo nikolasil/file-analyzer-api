06811fa855a0032274a6ee3414ee5af079824ece
nikolasil <nikolasil2000@gmail.com>
2022-12-24 14:24:43 +0200

    first commit

---
+++ src/main/java/com/example/fileanalyzerapi/FileAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileanalyzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAnalyzerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileAnalyzerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fileanalyzerapi/FileAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileanalyzerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAnalyzerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
